7ba9d01e700f92736af13f0bb7b70e1c717a5a72
murodjon <toshtemirovmurodjon03@gmail.com>
2022-06-08 11:25:31 +0500

    Alisher sayohati

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-var userTicket = document.querySelector(".heading")
-
 var userSum = +prompt("Pulingizni kiriting")
 
+var userTicket = document.querySelector(".heading")
+
 if(userSum >= 9666000){
+    // console.log("Oq yo’l, Alisher!");
     userTicket.textContent = "Oq yo’l, Alisher!"
 }else{
+    // console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan.");
     userTicket.textContent = "Alisher, ozgina sabr qilish kerak bo’lar ekan."
 }
